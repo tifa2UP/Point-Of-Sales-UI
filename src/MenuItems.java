4f2cbbd7df06a58fe6d6e25e74038361e79062fb
tifa2UP <tifa3333@gmail.com>
2016-04-15 09:25:49 -0700

    Created Menu Items

---
+++ src/MenuItems.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Abdellatif on 4/15/2016.
+ */
+public class MenuItems {
+    private ArrayList<Item> menuItems;
+
+    public MenuItems(int numberOfItems){
+        menuItems = new ArrayList<>();
+    }
+
+    public void addItem (Item item){
+        menuItems.add(item);
+    }
+
+    public int getNumberOfItems(){
+        return menuItems.size();
+    }
+
+}
