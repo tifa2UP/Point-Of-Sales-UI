02820bc3ffcce4764e8b4e4788fbda9b44be6db9
tifa2UP <tifa3333@gmail.com>
2016-04-16 18:32:16 -0700

    Successfully parsed the text file into the system

--- src/McPatterns.java
+++ src/McPatterns.java
@@ -14,13 +14,14 @@ public class McPatterns {
             while (input.hasNextLine()){
                 String s = input.nextLine();
                 String itemName = s.substring(0, s.indexOf('|'));
-                double itemPrice = Double.parseDouble(s.substring('|'));
+                double itemPrice = Double.parseDouble(s.substring(s.indexOf('|') + 1));
                 itemsList.addItem(new Item(itemName, itemPrice));
             }
         }
         catch (Exception e){
-
+            e.printStackTrace();
         }
+
         McPatternsGUI gui = new McPatternsGUI(new McPatternsPresenter());
     }
 }
